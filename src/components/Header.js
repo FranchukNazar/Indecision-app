71ae7db10f257a49cf97ab772067041ccb821072
Franuk <nazfranuk@gmail.com>
2018-06-04 18:06:00 +0300

    add style to all app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
-        <h1>{props.tittle}</h1>
-        {props.subtittle && <h2>{props.subtittle}</h2>}
+    <div className="header">
+        <div className="container">
+            <h1 className="header__tittle">{props.tittle}</h1>
+            {props.subtittle && <h2 className="header__subtittle">{props.subtittle}</h2>}
+        </div>
     </div>
 );
 
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -72,21 +72,25 @@ class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtittle={subtittle} />
-                <Action
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOption={this.handleAddOption}
+                        />
+                    </div>
+                </div>
                 <OptionModal
-                    handleClearSelectedOption = {this.handleClearSelectedOption} 
-                    selectedOption = {this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
+                    selectedOption={this.state.selectedOption}
                 />
             </div>
         );
