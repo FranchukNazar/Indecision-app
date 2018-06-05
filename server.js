de3e91c422083ea03efacdc41d2daeb3a93a1926
Franuk <nazfranuk@gmail.com>
2018-06-05 12:47:00 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}!`);
+});
